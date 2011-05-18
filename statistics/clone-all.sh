f8bf64d615f900e11af2ae4e811a724dd8c37bb6
Gonzalo Odiard <godiard@sugarlabs.org>
2011-05-18 10:53:30 -0300

    Add information about repository.
    Now clone-all.sh works for every activity except scratch.

--- statistics/clone-all.sh
+++ statistics/clone-all.sh
@@ -1,10 +1,12 @@
-for LINE in `cat ./activities-data.txt`
+for LINE in `grep -v ^# ./activities-data.txt`
 do
 	ACTIVITY=`echo $LINE | awk -F '|' '{printf("%s",$1)}'`
+	GIT=`echo $LINE | awk -F '|' '{printf("%s",$5)}'`
 	REPO=`echo $LINE | awk -F '|' '{printf("%s",$2)}'`
+
     echo $ACTIVITY
-    if [ -d $ACTIVITY/mainline/.git/ ]; then
-    	cd $ACTIVITY/mainline
+    if [ -d $ACTIVITY/$GIT.git/ ]; then
+    	cd $ACTIVITY/$GIT
         git pull
         cd ..
     else
