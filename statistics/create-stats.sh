8eae1397859df3980fe92f9647c0d706c5a17efe
Gonzalo Odiard <godiard@sugarlabs.org>
2011-05-24 11:15:38 -0300

    Remove old files

--- statistics/create-stats.sh
+++
@@ -1,7 +0,0 @@
-for LINE in `cat ./activities-data.txt`
-do
-	ACTIVITY=`echo $LINE | awk -F '|' '{printf("%s",$1)}'`
-	cd $ACTIVITY
-    ../git-stats/gitstats/gitstats mainline/ STATS
-    cd ..
-done
