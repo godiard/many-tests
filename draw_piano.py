b7540688a5867e36402b0350b8a70bbe8e52eb5b
Gonzalo Odiard <godiard@gmail.com>
2012-01-01 17:23:57 -0300

    Change the key selected with mouse movement

--- draw_piano.py
+++ draw_piano.py
@@ -30,12 +30,20 @@ class PianoKeyboard(gtk.DrawingArea):
         self._add_c = add_c
         self._labels = labels
         self._pressed_key = None
+        self.font_size = 20
         super(PianoKeyboard, self).__init__()
+        # info needed to check keys positions
+        self._white_keys = [0, 2, 4, 5, 7, 9, 11]
+        self._l_keys_areas = [0, 3]
+        self._t_keys_areas = [1, 4, 5]
+        self._j_keys_areas = [2, 6]
         self.connect('expose_event', self.expose)
         self.connect('button_press_event', self.__button_press_cb)
         self.connect('button_release_event', self.__button_release_cb)
+        self.connect('motion_notify_event', self.__motion_notify_cb)
         self.set_events(gtk.gdk.EXPOSURE_MASK | gtk.gdk.BUTTON_PRESS_MASK | \
-                gtk.gdk.BUTTON_RELEASE_MASK)
+                gtk.gdk.BUTTON_RELEASE_MASK | gtk.gdk.POINTER_MOTION_MASK | \
+                gtk.gdk.POINTER_MOTION_HINT_MASK)
 
     def calculate_sizes(self, width):
         self._width = width
@@ -50,43 +58,85 @@ class PianoKeyboard(gtk.DrawingArea):
     def __button_press_cb(self, widget, event):
         if event.button == 1:
             x, y = event.x, event.y
-            if y > self._height:
+            key_found = self.__get_key_at_position(x, y)
+            if key_found is None:
                 return True
-            octave_clicked = int(x / self._octave_width)
-            key_area = int((x % self._octave_width) / self._key_width)
-            click_x = int(x % self._key_width)
-            white_keys = [0, 2, 4, 5, 7, 9, 11]
-            l_keys_areas = [0, 3]
-            t_keys_areas = [1, 4, 5]
-            j_keys_areas = [2, 6]
-            if y > self._black_keys_height or \
-                (self._add_c and x > self._width - self._key_width):
-                key_clicked = white_keys[key_area]
             else:
-                # check black key at the right
-                key_clicked = -1
-                if key_area in l_keys_areas or \
-                    key_area in t_keys_areas:
-                    if click_x > self._key_width * 2 / 3:
-                        key_clicked = white_keys[key_area] + 1
-                # check black key at the left
-                if key_clicked == -1 and \
-                    key_area in j_keys_areas or \
-                    key_area in t_keys_areas:
-                    if click_x < self._key_width * 1 / 3:
-                        key_clicked = white_keys[key_area] - 1
-                if key_clicked == -1:
-                    key_clicked = white_keys[key_area]
-            self._pressed_key = (octave_clicked, key_clicked)
-            self.emit('key_pressed', octave_clicked, key_clicked,
-                    self._labels[octave_clicked][key_clicked])
-            self.queue_draw()
+                self._pressed_key = key_found
+                octave_clicked = key_found[0]
+                key_clicked = key_found[1]
+                self.emit('key_pressed', octave_clicked, key_clicked,
+                        self.get_label(octave_clicked, key_clicked))
+                self.queue_draw()
+                return True
+
+    def __motion_notify_cb(self, widget, event):
+        # only continue if the button is pressed
+        if self._pressed_key is None:
             return True
 
+        # if this is a hint, then let's get all the necessary
+        # information, if not it's all we need.
+        if event.is_hint:
+            x, y, state = event.window.get_pointer()
+        else:
+            x = event.x
+            y = event.y
+            state = event.state
+
+        key_found = self.__get_key_at_position(x, y)
+        if key_found != self._pressed_key:
+            self.emit('key_released', self._pressed_key[0],
+                self._pressed_key[1], self.get_label(*self._pressed_key))
+
+            self._pressed_key = key_found
+            if key_found is not None:
+                octave_clicked = key_found[0]
+                key_clicked = key_found[1]
+                self.emit('key_pressed', octave_clicked, key_clicked,
+                        self.get_label(octave_clicked, key_clicked))
+            self.queue_draw()
+
+    def __get_key_at_position(self, x, y):
+        if y > self._height:
+            return None
+        octave_found = int(x / self._octave_width)
+        key_area = int((x % self._octave_width) / self._key_width)
+        click_x = int(x % self._key_width)
+        if y > self._black_keys_height or \
+            (self._add_c and x > self._width - self._key_width):
+            key_found = self._white_keys[key_area]
+        else:
+            # check black key at the right
+            key_found = -1
+            if key_area in self._l_keys_areas or \
+                key_area in self._t_keys_areas:
+                if click_x > self._key_width * 2 / 3:
+                    key_found = self._white_keys[key_area] + 1
+            # check black key at the left
+            if key_found == -1 and \
+                key_area in self._j_keys_areas or \
+                key_area in self._t_keys_areas:
+                if click_x < self._key_width * 1 / 3:
+                    key_found = self._white_keys[key_area] - 1
+            if key_found == -1:
+                key_found = self._white_keys[key_area]
+        return (octave_found, key_found)
+
     def __button_release_cb(self, widget, event):
-        self._pressed_key = None
-        self.emit('key_released', -1, -1, '')
-        self.queue_draw()
+        if self._pressed_key is not None:
+            self.emit('key_released', self._pressed_key[0],
+                    self._pressed_key[1], self.get_label(*self._pressed_key))
+            self._pressed_key = None
+            self.queue_draw()
+
+    def get_label(self, octave, key):
+        if self._labels is None:
+            return ""
+        try:
+            return self._labels[octave][key]
+        except:
+            return ""
 
     def expose(self, widget, event):
         rect = self.get_allocation()
@@ -102,8 +152,7 @@ class PianoKeyboard(gtk.DrawingArea):
         # calculate text height
         ctx.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
                 cairo.FONT_WEIGHT_NORMAL)
-        # TODO: configure or calculate
-        ctx.set_font_size(20)
+        ctx.set_font_size(self.font_size)
         x_bearing, y_bearing, width, height, x_advance, y_advance = \
                 ctx.text_extents('M')
         self._text_height = height
@@ -118,6 +167,8 @@ class PianoKeyboard(gtk.DrawingArea):
             self.draw_last_C(ctx, n + 1)
         if self._pressed_key is not None:
             octave, key = self._pressed_key
+            if octave == -1 or (octave == self._octaves and key > 0):
+                return
             if key == 0:
                 if octave < self._octaves:
                     self.draw_C(ctx, octave, True)
@@ -175,71 +226,71 @@ class PianoKeyboard(gtk.DrawingArea):
     def draw_C(self, ctx, octave_number, highlighted=False):
         x = self._key_width * (octave_number * 7)
         self.draw_key_L(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 0, False)
+        self._draw_label(ctx, x, octave_number, 0, False, highlighted)
 
     def draw_CB(self, ctx, octave_number, highlighted=False):
         x = self._key_width * (octave_number * 7) + self._key_width * 2 / 3
         self.draw_black(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 1, True)
+        self._draw_label(ctx, x, octave_number, 1, True, highlighted)
 
     def draw_D(self, ctx, octave_number, highlighted=False):
         x = self._key_width + self._key_width * (octave_number * 7)
         self.draw_key_T(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 2, False)
+        self._draw_label(ctx, x, octave_number, 2, False, highlighted)
 
     def draw_DB(self, ctx, octave_number, highlighted=False):
         x = self._key_width + self._key_width * 2 / 3 + \
                 self._key_width * (octave_number * 7)
         self.draw_black(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 3, True)
+        self._draw_label(ctx, x, octave_number, 3, True, highlighted)
 
     def draw_E(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 2 + self._key_width * (octave_number * 7)
         self.draw_key_J(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 4, False)
+        self._draw_label(ctx, x, octave_number, 4, False, highlighted)
 
     def draw_F(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 3 + self._key_width * (octave_number * 7)
         self.draw_key_L(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 5, False)
+        self._draw_label(ctx, x, octave_number, 5, False, highlighted)
 
     def draw_FB(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 3 + self._key_width * 2 / 3 + \
                 self._key_width * (octave_number * 7)
         self.draw_black(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 6, True)
+        self._draw_label(ctx, x, octave_number, 6, True, highlighted)
 
     def draw_G(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 4 + self._key_width * (octave_number * 7)
         self.draw_key_T(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 7, False)
+        self._draw_label(ctx, x, octave_number, 7, False, highlighted)
 
     def draw_GB(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 4 + self._key_width * 2 / 3 + \
                 self._key_width * (octave_number * 7)
         self.draw_black(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 8, True)
+        self._draw_label(ctx, x, octave_number, 8, True, highlighted)
 
     def draw_A(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 5 + self._key_width * (octave_number * 7)
         self.draw_key_T(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 9, False)
+        self._draw_label(ctx, x, octave_number, 9, False, highlighted)
 
     def draw_AB(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 5 + self._key_width * 2 / 3 + \
                 self._key_width * (octave_number * 7)
         self.draw_black(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 10, True)
+        self._draw_label(ctx, x, octave_number, 10, True, highlighted)
 
     def draw_B(self, ctx, octave_number, highlighted=False):
         x = self._key_width * 6 + self._key_width * (octave_number * 7)
         self.draw_key_J(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 11, False)
+        self._draw_label(ctx, x, octave_number, 11, False, highlighted)
 
     def draw_last_C(self, ctx, octave_number, highlighted=False):
         x = self._key_width * (octave_number * 7)
         self.draw_key_simple(ctx, x, highlighted)
-        self._draw_label(ctx, x, octave_number, 0, False)
+        self._draw_label(ctx, x, octave_number, 0, False, highlighted)
 
     def draw_key_L(self, ctx, x, highlighted):
         ctx.save()
@@ -329,7 +380,8 @@ class PianoKeyboard(gtk.DrawingArea):
         ctx.set_source_rgb(*stroke)
         ctx.stroke()
 
-    def _draw_label(self, ctx, x, octave_number, position, black_key):
+    def _draw_label(self, ctx, x, octave_number, position, black_key,
+                highlighted):
         #print "Dibujando ",text
         if self._labels is not None:
             text = self._labels[octave_number][position]
@@ -338,7 +390,10 @@ class PianoKeyboard(gtk.DrawingArea):
             if black_key:
                 x_text = x + self._key_width * 1 / 3 - (width / 2 + x_bearing)
                 y_text = self._black_keys_height - (self._text_height * 2)
-                stroke = (1, 1, 1)
+                if highlighted:
+                    stroke = (0, 0, 0)
+                else:
+                    stroke = (1, 1, 1)
             else:
                 x_text = x + self._key_width / 2 - (width / 2 + x_bearing)
                 y_text = self._height - (self._text_height * 2)
