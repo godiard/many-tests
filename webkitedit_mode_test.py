476cfbb07e1563d16ddd9d8394b846cce3b85417
Gonzalo Odiard <godiard@gmail.com>
2015-04-22 15:56:53 -0300

    Make a class in the test of edition in a html using webkit

--- webkitedit_mode_test.py
+++ webkitedit_mode_test.py
@@ -3,30 +3,37 @@ import os
 from gi.repository import WebKit
 from gi.repository import Gtk
 
-def _destroy_cb(widget):
-    Gtk.main_quit()
 
-def toggle_highlight(button, webview):
-    #webview.set_editable(True)
-    webview.execute_script(
-      'document.execCommand("backColor", false, "yellow");')
-    #webview.set_editable(False)
+class WebKitHighlighter(Gtk.Window):
 
-local_path = os.path.dirname(os.path.realpath(__file__))
-window = Gtk.Window()
-window.set_default_size(800, 640)
-window.connect("destroy", _destroy_cb)
+    def __init__(self):
+        local_path = os.path.dirname(os.path.realpath(__file__))
+        Gtk.Window.__init__(self)
+        self.set_default_size(800, 640)
+        self.connect("destroy", self.__destroy_cb)
 
-webview = WebKit.WebView()
+        webview = WebKit.WebView()
 
-vbox = Gtk.VBox()
-highlight_bt = Gtk.ToggleButton('Highlight')
-highlight_bt.connect('toggled', toggle_highlight, webview)
-vbox.pack_start(highlight_bt, False, False, 10)
+        vbox = Gtk.VBox()
+        highlight_bt = Gtk.ToggleButton('Highlight')
+        highlight_bt.connect('toggled', self.__toggle_highlight_cb, webview)
+        vbox.pack_start(highlight_bt, False, False, 10)
 
-webview.load_uri('file://%s/liebre_y_tortuga.html' % local_path)
-vbox.pack_start(webview, True, True, 10)
+        webview.load_uri('file://%s/liebre_y_tortuga.html' % local_path)
+        vbox.pack_start(webview, True, True, 10)
 
-window.add(vbox)
-window.show_all()
+        self.add(vbox)
+        self.show_all()
+
+    def __destroy_cb(self, widget):
+        Gtk.main_quit()
+
+    def __toggle_highlight_cb(self, button, webview):
+        webview.set_editable(True)
+        webview.execute_script(
+            'document.execCommand("backColor", false, "yellow");')
+        webview.set_editable(False)
+
+winow = WebKitHighlighter()
+
 Gtk.main()
